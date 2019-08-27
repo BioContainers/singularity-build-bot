3ee6e20d2e24cc65fa4e074da54c7ce4eedb8caf
Björn Grüning <bjoern@gruenings.eu>
2019-08-27 17:25:36 +0200

    silence [ci skip]

--- install_singularity.sh
+++ install_singularity.sh
@@ -1,9 +1,9 @@
 sudo rm -rf /usr/local/go # remove preinstalled go
-sudo apt-get update && sudo apt-get install -y build-essential libssl-dev uuid-dev libgpgme11-dev squashfs-tools libseccomp-dev pkg-config
+sudo apt-get update -qq && sudo apt-get -qq install -y build-essential libssl-dev uuid-dev libgpgme11-dev squashfs-tools libseccomp-dev pkg-config
 
 # install go
-wget https://dl.google.com/go/go1.12.9.linux-amd64.tar.gz
-sudo tar -C /usr/local -xzvf go1.12.9.linux-amd64.tar.gz
+wget -q https://dl.google.com/go/go1.12.9.linux-amd64.tar.gz
+sudo tar -C /usr/local -xzf go1.12.9.linux-amd64.tar.gz
 rm go1.12.9.linux-amd64.tar.gz
 
 # get deps
@@ -13,7 +13,7 @@ go get -d github.com/sylabs/singularity && exit 0
 # install singularity
 mkdir -p $HOME/go/src/github.com/sylabs
 cd $HOME/go/src/github.com/sylabs
-wget https://github.com/sylabs/singularity/releases/download/v3.3.0/singularity-3.3.0.tar.gz
+wget -q https://github.com/sylabs/singularity/releases/download/v3.3.0/singularity-3.3.0.tar.gz
 tar -xzf singularity-3.3.0.tar.gz
 cd ./singularity
 ./mconfig
