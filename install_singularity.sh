ade7aea4a5cb4893c18afcbbe3d82f0896bd3fe8
Simon Bray <32272674+simonbray@users.noreply.github.com>
2019-08-28 13:46:34 +0200

    update logging [skip ci]

--- install_singularity.sh
+++ install_singularity.sh
@@ -3,21 +3,25 @@ sudo apt-get update -qq && sudo apt-get -qq install -y build-essential libssl-de
 echo "Requirements installed. Installing Go..."
 
 # install go
+echo "Downloading Go from https://dl.google.com/go/go1.12.9.linux-amd64.tar.gz and untarring to /usr/local..."
 wget -q https://dl.google.com/go/go1.12.9.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.12.9.linux-amd64.tar.gz
 rm go1.12.9.linux-amd64.tar.gz
-echo "Go successfully installed. Installing Singularity..."
+echo "Go successfully installed."
 
 # get deps
+echo "Getting Singularity dependencies with go get..."
 go get -u github.com/golang/dep/cmd/dep > /dev/null 2>&1
 go get -d github.com/sylabs/singularity > /dev/null 2>&1 && exit 0 
 
 # install singularity
+echo "Downloading Singularity from https://github.com/sylabs/singularity/releases/download/v3.3.0/singularity-3.3.0.tar.gz and untarring..."
 mkdir -p $HOME/go/src/github.com/sylabs
 cd $HOME/go/src/github.com/sylabs
 wget -q https://github.com/sylabs/singularity/releases/download/v3.3.0/singularity-3.3.0.tar.gz
 tar -xzf singularity-3.3.0.tar.gz
 cd ./singularity
+echo "Running mconfig, make, and make install..."
 ./mconfig 1> /dev/null
 make -C ./builddir 1> /dev/null
 sudo make -C ./builddir install 1> /dev/null
