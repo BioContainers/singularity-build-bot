c9bca660f0ab78ed6136073daadccf34dde140bf
Moritz E. Beber <midnighter@posteo.net>
2022-03-08 01:35:40 +0100

    fix: follow next page of quay.io images
    refactor: completely refactor script for Python 3.8+

--- get_container_list.py
+++ get_container_list.py
@@ -1,91 +1,350 @@
-import re
-import requests
+#!/usr/bin/env python
+import argparse
+import asyncio
+import logging
+from enum import Enum
+from functools import partial
+from html.parser import HTMLParser
+from pathlib import Path
+from typing import List, Tuple, Dict, Optional, Iterable
 
-try:
-    from html.parser import HTMLParser
-except ImportError:  # python 2
-    from HTMLParser import HTMLParser
+import aiometer
+import httpx
+import pydantic
+import rich.progress as rprog
+import tenacity
+from rich.logging import RichHandler
 
-QUAY_API_ENDPOINT = 'https://quay.io/api/v1/repository'
 
-def get_quay_containers(repository='biocontainers'):
-    """
-    Get all quay containers in the biocontainers repo
-    """
-    containers = []
+logger = logging.getLogger()
 
-    repos_parameters = {'public': 'true', 'namespace': repository}
-    repos_headers = {'Accept-encoding': 'gzip', 'Accept': 'application/json'}
-    repos_response = requests.get(
-        QUAY_API_ENDPOINT, headers=repos_headers, params=repos_parameters, timeout=12)
 
-    repos = repos_response.json()['repositories']
+class RepositoryKind(str, Enum):
 
-    for repo in repos:
-        # logging.info(repo)
-        tags_response = requests.get(
-            "%s/%s/%s" % (QUAY_API_ENDPOINT, repository, repo['name']))
-        tags = tags_response.json()['tags']
-        for tag in tags:
-            containers.append('%s:%s' % (repo['name'], tag))
+    image = "image"
 
-    return containers
 
-def get_singularity_containers(url="https://depot.galaxyproject.org/singularity/new/"):
-    """
-    Get all existing singularity containers from "https://depot.galaxyproject.org/singularity/"
-    """
-    class GetContainerNames(HTMLParser):  # small parser which gets list of containers
-        def __init__(self):
-            HTMLParser.__init__(self)
-            self.containers = []
+class RepositoryState(str, Enum):
 
-        def handle_starttag(self, tag, attrs):
-            try:
-                for attr in attrs:
-                    if attr[0] == 'href' and re.match('.*%3A.*', attr[1]):
-                        self.containers.append(attr[1].replace('%3A', ':'))
-            except IndexError:
-                pass
+    NORMAL = "NORMAL"
 
-    parser = GetContainerNames()
-    index = requests.get(url)
-    parser.feed(index.text)
-    return parser.containers
 
-def check_multiple_singularity_directories(urls):
-    """
-    For a list of urls, get Singularity containers for each and find the union
-    """
-    containers = []
-    for url in urls:
-        containers += get_singularity_containers(url)
-    return set(containers)
+class Repository(pydantic.BaseModel):
 
-def get_missing_containers(quay_list, singularity_list, blacklist_file=None):
-    r"""
-    Return list of quay containers that do not exist as singularity containers. Files stored in a blacklist will be ignored
-    """
-    blacklist = []
-    if blacklist_file:
-        blacklist = open(blacklist_file).read().splitlines()
-    rgx = "(" + ")|(".join(blacklist) + ")"
-    return [n for n in quay_list if n not in singularity_list and not re.match(rgx, n)]
+    namespace: str
+    name: str
+    is_public: bool
+    kind: RepositoryKind
+    state: RepositoryState
 
-############
-### MAIN ###
-############
 
-SINGULARITY_DIRECTORIES = ["https://depot.galaxyproject.org/singularity/new/", "https://depot.galaxyproject.org/singularity/"] # dirs to check for containers
+class RepositoryListResponse(pydantic.BaseModel):
 
-print('Getting list of containers to build. This may take a while...')
-quay = get_quay_containers()
-sing = check_multiple_singularity_directories(SINGULARITY_DIRECTORIES)
+    repositories: List[Repository]
+    next_page: Optional[str] = None
 
-lst = get_missing_containers(quay, sing, 'skip.list')
-lst = sorted([c for c in lst if 'bioconductor' not in c]) + sorted([c for c in lst if 'bioconductor' in c])
 
-with open('build.sh', 'w') as f:
-    for c_no, container in enumerate(lst):
-        f.write(f"sudo singularity build {container} docker://quay.io/biocontainers/{container} > /dev/null 2>&1 && rsync -azq -e 'ssh -i ssh_key -o StrictHostKeyChecking=no' ./{container} singularity@depot.galaxyproject.org:/srv/nginx/depot.galaxyproject.org/root/singularity/ && rm {container} && echo 'Container {c_no + 1} ({container}) of {len(lst)} built.'\n")
-print('{} containers found. Building...'.format(len(lst)))
+class RepositoryTag(pydantic.BaseModel):
+
+    name: str
+
+
+class SingleRepositoryResponse(Repository):
+
+    tags: Dict[str, RepositoryTag]
+
+
+class ContainerImageParser(HTMLParser):
+    """Define a parser for container names and tags."""
+
+    def __init__(self, **kwargs) -> None:
+        """Initialize a default container parser."""
+        super().__init__(**kwargs)
+        self._images = []
+
+    @property
+    def images(self) -> List[str]:
+        """Return the list of parsed container images."""
+        return self._images.copy()
+
+    def handle_starttag(self, tag: str, attrs: List[Tuple[str, str]]) -> None:
+        if tag != "a":
+            return
+        for attr, value in attrs:
+            if attr != "href":
+                continue
+            if ":" in value:
+                self._images.append(value)
+
+
+class QuayImageFetcher:
+    @classmethod
+    async def fetch_all(
+        cls,
+        api_url: str,
+        repository: str = "biocontainers",
+        params: Optional[Dict[str, str]] = None,
+        headers: Optional[Dict[str, str]] = None,
+    ) -> List[str]:
+        if headers is None:
+            headers = {
+                "Accept-Encoding": "gzip",
+                "Accept": "application/json",
+                "User-Agent": "singularity-build-bot",
+            }
+        if params is None:
+            params = {"public": "true", "repo_kind": "image"}
+        params["namespace"] = repository
+        async with httpx.AsyncClient(
+            base_url=api_url, headers=headers, timeout=httpx.Timeout(12)
+        ) as client:
+            names = await cls._fetch_names(client=client, params=params)
+            images = await cls._fetch_tags(
+                client=client, repository=repository, names=names
+            )
+        return images
+
+    @classmethod
+    async def _fetch_names(
+        cls, client: httpx.AsyncClient, params: Dict[str, str]
+    ) -> List[str]:
+        names = []
+        logger.debug("Fetching batch 1/?")
+        repos = await cls._fetch_repository_list(client=client, params=params)
+        names.extend((repo.name for repo in repos.repositories))
+        counter = 2
+        while repos.next_page:
+            logger.debug("Fetching batch %d/?", counter)
+            repos = await cls._fetch_repository_list(
+                client=client, params={**params, "next_page": repos.next_page}
+            )
+            names.extend((repo.name for repo in repos.repositories))
+            counter += 1
+        return names
+
+    @staticmethod
+    @tenacity.retry(
+        wait=tenacity.wait_random_exponential(),
+        stop=tenacity.stop_after_attempt(5),
+        reraise=True,
+        retry=tenacity.retry_if_exception_type(httpx.HTTPError),
+        before=tenacity.before_log(logger, logging.DEBUG),
+    )
+    async def _fetch_repository_list(
+        client: httpx.AsyncClient, params: Dict[str, str]
+    ) -> RepositoryListResponse:
+        response = await client.get("repository", params=params)
+        response.raise_for_status()
+        return RepositoryListResponse.parse_obj(response.json())
+
+    @classmethod
+    async def _fetch_tags(
+        cls,
+        client: httpx.AsyncClient,
+        repository: str,
+        names: List[str],
+        max_concurrency: int = 10,
+        max_per_second: int = 10,
+    ) -> List[str]:
+        requests = [
+            client.build_request(method="GET", url=f"repository/{repository}/{name}")
+            for name in names
+        ]
+        images = []
+        with cls._progress_bar() as pbar:
+            task = pbar.add_task(description="Repository Tags", total=len(requests))
+            async with aiometer.amap(
+                partial(cls._fetch_single_repository, client),
+                requests,
+                max_at_once=max_concurrency,
+                max_per_second=max_per_second,
+            ) as results:
+                async for repo in results:  # type: SingleRepositoryResponse
+                    images.extend((f"{repo.name}:{tag}" for tag in repo.tags))
+                    pbar.update(task, advance=1)
+        return images
+
+    @classmethod
+    def _progress_bar(cls) -> rprog.Progress:
+        """Create a rich progress bar."""
+        return rprog.Progress(
+            rprog.TextColumn("[bold blue]{task.description}", justify="right"),
+            rprog.BarColumn(bar_width=None),
+            "[progress.percentage]{task.completed:,}/{task.total:,}({task.percentage:>3.1f}%)",
+            " ",
+            rprog.TimeRemainingColumn(),
+            " ",
+            rprog.TimeElapsedColumn(),
+        )
+
+    @staticmethod
+    @tenacity.retry(
+        wait=tenacity.wait_random_exponential(),
+        stop=tenacity.stop_after_attempt(5),
+        reraise=True,
+        retry=tenacity.retry_if_exception_type(httpx.HTTPError),
+        before=tenacity.before_log(logger, logging.DEBUG),
+    )
+    async def _fetch_single_repository(
+        client: httpx.AsyncClient, request: httpx.Request
+    ) -> SingleRepositoryResponse:
+        response = await client.send(request=request)
+        response.raise_for_status()
+        return SingleRepositoryResponse.parse_obj(response.json())
+
+
+class SingularityImageFetcher:
+    @classmethod
+    def fetch_all(
+        cls,
+        urls: Iterable[str],
+        headers: Optional[Dict[str, str]] = None,
+    ) -> List[str]:
+        if headers is None:
+            headers = {
+                "Accept-Encoding": "gzip",
+                "Accept": "text/html",
+                "User-Agent": "singularity-build-bot",
+            }
+        images = []
+        with httpx.Client(headers=headers) as client:
+            for url in urls:
+                parser = ContainerImageParser()
+                if response := cls._fetch_images(client=client, url=url):
+                    parser.feed(response)
+                    images.extend(parser.images)
+                else:
+                    logger.warning("No images found at '%s'.", url)
+        return images
+
+    @staticmethod
+    @tenacity.retry(
+        wait=tenacity.wait_random_exponential(),
+        stop=tenacity.stop_after_attempt(5),
+        reraise=True,
+        retry=tenacity.retry_if_exception_type(httpx.HTTPError),
+        before=tenacity.before_log(logger, logging.DEBUG),
+    )
+    def _fetch_images(client: httpx.Client, url: str) -> Optional[str]:
+        response = client.get(url=url)
+        if response.status_code == 404:
+            return
+        response.raise_for_status()
+        return response.text
+
+
+def get_new_images(
+    quay_images: Iterable[str],
+    singularity_images: Iterable[str],
+    denylist: Iterable[str],
+) -> List[str]:
+    denylist = tuple(denylist)
+    result = frozenset(quay_images) - frozenset(singularity_images)
+    # Filter new images using the deny list.
+    # FIXME: Is it necessary to sort bioconductor images to the end as before?
+    return sorted(
+        filter(
+            lambda image: not any(image.startswith(entry) for entry in denylist),
+            result,
+        )
+    )
+
+
+def parse_denylist(filename: Path) -> List[str]:
+    with filename.open() as handle:
+        return [entry for line in handle.readlines() if (entry := line.strip())]
+
+
+def generate_build_script(filename: Path, images: List[str]) -> None:
+    with filename.open("w") as handle:
+        for idx, name in enumerate(images, start=1):
+            handle.write(
+                f"sudo singularity build {name} docker://quay.io/biocontainers/{name} > /dev/null 2>&1 && rsync -azq -e 'ssh -i ssh_key -o StrictHostKeyChecking=no' ./{name} singularity@depot.galaxyproject.org:/srv/nginx/depot.galaxyproject.org/root/singularity/ && rm {name} && echo 'Container {name} built ({idx}/{len(images)}).'\n"
+            )
+
+
+def parse_args(argv: Optional[List[str]] = None) -> argparse.Namespace:
+    """Define and immediately parse command line arguments."""
+    parser = argparse.ArgumentParser(
+        description="Find new BioContainers images for which no Singularity image "
+        "exists yet and generate a build script for them.",
+    )
+    default_denylist = Path("skip.list")
+    parser.add_argument(
+        "--denylist",
+        metavar="PATH",
+        default=default_denylist,
+        type=Path,
+        help=f"File containing image names to skip; one per line (default "
+        f"'{default_denylist}').",
+    )
+    default_build_script = Path("build.sh")
+    parser.add_argument(
+        "--build-script",
+        metavar="PATH",
+        default=default_build_script,
+        type=Path,
+        help=f"Output the Singularity build script (default '{default_build_script}').",
+    )
+    default_quay_api = "https://quay.io/api/v1/"
+    parser.add_argument(
+        "--quay-api",
+        metavar="URL",
+        type=str,
+        default=default_quay_api,
+        help=f"The base URL for the quay.io API; must end with a '/' (default "
+        f"'{default_quay_api}').",
+    )
+    default_singularity_urls = ",".join(
+        [
+            "https://depot.galaxyproject.org/singularity/new/",
+            "https://depot.galaxyproject.org/singularity/",
+        ]
+    )
+    parser.add_argument(
+        "--singularity",
+        metavar="URL[,URL]",
+        type=str,
+        default=default_singularity_urls,
+        help=f"One or more URLs for Singularity depots; must end with a '/' and be "
+        f"separated by commas (default '{default_singularity_urls}').",
+    )
+    default_log_level = "INFO"
+    parser.add_argument(
+        "-l",
+        "--log-level",
+        help=f"The desired log level (default {default_log_level}).",
+        choices=("CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG"),
+        default=default_log_level,
+    )
+    return parser.parse_args(argv)
+
+
+def main(argv: Optional[List[str]] = None) -> None:
+    args = parse_args(argv=argv)
+    logging.basicConfig(
+        level=args.log_level,
+        format="%(message)s",
+        datefmt="[%X]",
+        handlers=[RichHandler(markup=True, rich_tracebacks=True)],
+    )
+    assert args.denylist.is_file(), f"File not found '{args.denylist}'."
+    args.build_script.parent.mkdir(parents=True, exist_ok=True)
+    logger.info("Fetching quay.io BioContainers images.")
+    quay_images = asyncio.run(QuayImageFetcher.fetch_all(api_url=args.quay_api))
+    logger.info("Fetching Singularity BioContainers images.")
+    singularity_images = SingularityImageFetcher.fetch_all(
+        urls=args.singularity.split(",")
+    )
+    logger.info("Parsing container image deny list.")
+    denylist = parse_denylist(args.denylist)
+    images = get_new_images(quay_images, singularity_images, denylist)
+    if not images:
+        logger.warning("No new images found.")
+        return
+    logger.info("%d new images found. Generating build script.", len(images))
+    generate_build_script(args.build_script, images)
+
+
+if __name__ == "__main__":
+    main()
